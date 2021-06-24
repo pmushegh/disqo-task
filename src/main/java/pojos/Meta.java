8a6d1b8a44300b455e2e2fad875aff9443070173
Mushegh Petrosyan <p.mushegh@gmail.com>
2021-06-24 15:51:26 +0400

    Add Users pojo

---
+++ src/main/java/pojos/Meta.java
@@ -0,0 +1,22 @@
+package pojos;
+
+public class Meta
+{
+    private Pagination pagination;
+
+    public Pagination getPagination ()
+    {
+        return pagination;
+    }
+
+    public void setPagination (Pagination pagination)
+    {
+        this.pagination = pagination;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [pagination = "+pagination+"]";
+    }
+}
