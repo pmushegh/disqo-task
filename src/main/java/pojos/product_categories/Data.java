5c5631f9d0b286fd20d612fe9a4ac6d8c4f32ddf
Mushegh Petrosyan <p.mushegh@gmail.com>
2021-06-24 16:19:10 +0400

    Add Product-Categories pojo

---
+++ src/main/java/pojos/product_categories/Data.java
@@ -0,0 +1,46 @@
+package pojos.product_categories;
+
+public class Data
+{
+    private String category_id;
+
+    private String product_id;
+
+    private String id;
+
+    public String getCategory_id ()
+    {
+        return category_id;
+    }
+
+    public void setCategory_id (String category_id)
+    {
+        this.category_id = category_id;
+    }
+
+    public String getProduct_id ()
+    {
+        return product_id;
+    }
+
+    public void setProduct_id (String product_id)
+    {
+        this.product_id = product_id;
+    }
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [category_id = "+category_id+", product_id = "+product_id+", id = "+id+"]";
+    }
+}
---
+++ src/main/java/pojos/product_categories/ProductCategories.java
@@ -0,0 +1,48 @@
+package pojos.product_categories;
+
+import pojos.Meta;
+
+public class ProductCategories
+{
+    private String code;
+
+    private Data[] data;
+
+    private Meta meta;
+
+    public String getCode ()
+    {
+        return code;
+    }
+
+    public void setCode (String code)
+    {
+        this.code = code;
+    }
+
+    public Data[] getData ()
+    {
+        return data;
+    }
+
+    public void setData (Data[] data)
+    {
+        this.data = data;
+    }
+
+    public Meta getMeta ()
+    {
+        return meta;
+    }
+
+    public void setMeta (Meta meta)
+    {
+        this.meta = meta;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [code = "+code+", data = "+data+", meta = "+meta+"]";
+    }
+}
